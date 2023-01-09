43ca6edaaf33b09ed52a74251d27765b1f590513
Imran Chowdhury <96187725+imran2437@users.noreply.github.com>
2023-01-09 20:08:09 +0600

    Add files via upload

---
+++ School/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'School',
+    'version' : '15',
+    'summary': 'School',
+    'sequence': 10,
+    'description': """
+        School
+    """,
+
+    'website': 'https://www.odoo.com/app/invoicing',
+    'images' : [],
+    'depends' : [],
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+    'license': 'LGPL-3',
+}
