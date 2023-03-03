a20f9d8382fe5cd09d3b31947d895556bb653dc6
Imran Chowdhury <96187725+imran2437@users.noreply.github.com>
2023-03-03 20:47:37 +0600

    Delete School/models directory

--- School/models/__init__.py
+++
@@ -1,7 +0,0 @@
-
-
-from . import student
-from . import teacher
-from . import section
-from . import course
-from . import registration
--- School/models/section.py
+++
@@ -1,8 +0,0 @@
-
-from odoo import api, fields, models, _
-
-class Section(models.Model):
-    _name = 'school.section'
-    _description = 'section'
-    name = fields.Char('Section Name', required=True)
-    code = fields.Char('Code')
